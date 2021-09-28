53e454686111c63ec9b8d5a36ee7285993200e99
Wang Wei <wangwei1237@gmail.com>
2021-09-28 22:22:58 +0800

    将插件用到的css文件置于插件中，无需在hexo主题中单独管理插件的css文件

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var md = require('markdown-it')('commonmark');
+const path = require('path')
+const fs = require('hexo-fs')
+const STYLE_PATH = path.resolve(__dirname, './style.css')
 
 hexo.extend.filter.register('before_post_render', function (data) {
-  let strRegExp = /(<\!-- *)(note|info|todo|warning|attention|caution|failure|missing|fail|error) (.*?)--> *\n((.*\n)*?)(<\!-- end -->\n)/;
+  const style_content = fs.readFileSync(STYLE_PATH).toString()
+
+  let strRegExp = '(?<=^\n)(^!!! *)(note|info|todo|warning|attention|caution|failure|missing|fail|error)(.*\n)((^ {4}.*\n|^\n)+)';
   let admonitionRegExp = new RegExp(strRegExp, 'gmi');
 
   let strData;
@@ -12,18 +17,18 @@ hexo.extend.filter.register('before_post_render', function (data) {
     strData = data.content.replace(admonitionRegExp, function (matchStr, p1, p2, p3, p4) {
 
       p4 = p4.split(/\n|\r|\r\n/);
-
       let admonitionContent = '';
       for (const v of p4) {
-        admonitionContent += v.trim();
-        admonitionContent += '\n';3
+        admonitionContent += v.trim() + '\n';
       }
 
-      if (p3.replace(/\s+/g, '') === '""' || p3.replace(/\s+/g, '') === '') {
-        return '<div class="admonition ' + p2.toLowerCase() + '">' + md.render(admonitionContent) + '</div>\n\n';
+      if (p3.replace(/\s+/g, '') === '""') {
+        return '<style>' + style_content + '</style>\n\n' + 
+               '<div class="admonition ' + p2.toLowerCase() + '">' + md.render(admonitionContent) + '</div>\n\n';
       } else {
         p3 = p3.trim() === '' ? p2 : p3.replace(/(^ |")|("| $)/g, '');
-        return '<div class="admonition ' + p2.toLowerCase() + '"><p class="admonition-title">' + p3 + '</p>' + md.render(admonitionContent) + '</div>\n\n';
+        return '<style>' + style_content + '</style>\n\n' +  
+               '<div class="admonition ' + p2.toLowerCase() + '"><p class="admonition-title">' + p3 + '</p>' + md.render(admonitionContent) + '</div>\n\n';
       }
     });
     data.content = strData;
